08e78474a2d59c54d743b437dcaccfa94cb40965
Hamberfim <adhamlingithub@gmail.com>
2022-11-23 10:36:40 -0600

    started advanced

--- src/03_Advanced/039_implemntsKeyword.ts
+++ src/03_Advanced/039_implemntsKeyword.ts
@@ -1,1 +1,28 @@
-//
+// using the implements keyword
+type Animal = {
+  name: string;
+  voice(): string;
+};
+
+function log(animal: Animal) {
+  console.log(`Animal ${animal.name}: ${animal.voice()}`);
+}
+
+class Cat implements Animal {
+  constructor(public name: string) {}
+  voice(): string {
+    return "meow";
+  }
+}
+
+class Dog implements Animal {
+  constructor(public name: string) {}
+  voice(): string {
+    return "bark";
+  }
+}
+
+log(new Cat("Poo'yee"));
+log(new Dog("Sukha"));
+
+export {};
